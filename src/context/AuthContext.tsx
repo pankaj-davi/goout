a8da3310cc5b2abdb47be789d7728906408445e6
pankaj-davi <menariapankaj17@gmail.com>
2024-09-28 15:50:19 +0530

    update

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -6,6 +6,8 @@ import React, {
   useEffect,
 } from 'react';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {
   GoogleSignin,
   statusCodes,
@@ -22,15 +24,51 @@ interface AuthContextProps {
   isAuthloading: boolean;
   login: () => void;
   logout: () => void;
+  user: IUser | null;
+}
+
+interface IUser {
+  id: string;
+  name: string | null;
+  email: string;
+  photo: string | null;
+  familyName?: string | null; // Optional
+  givenName?: string | null; // Optional
+}
+
+interface CustomUser {
+  displayName: string | null;
+  email: string | null;
+  photoURL: string | null;
+  uid: string;
+  familyName?: string | null;
+  givenName?: string | null;
 }
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
 export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  const [isAuthloading, setIsAuthLoading] = useState<boolean>(false); // Initialize isAuthloading state
+  const [isAuthLoading, setIsAuthLoading] = useState<boolean>(true);
+  const [user, setUser] = useState<IUser | null>(null);
 
   useEffect(() => {
+    const checkUserStatus = async () => {
+      try {
+        const userData = await AsyncStorage.getItem('user');
+        if (userData) {
+          setUser(JSON.parse(userData));
+          setIsAuthenticated(true);
+        }
+      } catch (error) {
+        console.error('Error retrieving user data from storage:', error);
+      } finally {
+        setIsAuthLoading(false);
+      }
+    };
+
+    checkUserStatus();
+
     const unsubscribe = auth().onAuthStateChanged(user => {
       setIsAuthenticated(!!user);
     });
@@ -40,11 +78,41 @@ export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
 
   GoogleSignin.configure({
     webClientId:
-      '883572130459-u13r4chvsh1cknqa5ejrldcii655r51s.apps.googleusercontent.com', // Update with your webClientId
+      '883572130459-u13r4chvsh1cknqa5ejrldcii655r51s.apps.googleusercontent.com',
   });
 
+  const saveUserDataToFirestore = async (userData: CustomUser) => {
+    console.log('User Data before saving:', userData);
+    try {
+      const userDoc = {
+        uid: userData.uid || '',
+        name: userData.displayName || null,
+        email: userData.email || null,
+        photo: userData.photoURL || null,
+        familyName: userData.familyName || null,
+        givenName: userData.givenName || null,
+      };
+
+      await firestore()
+        .collection('users')
+        .doc(userData.uid)
+        .set(userDoc, {merge: true});
+      console.log('User data saved to Firestore successfully');
+    } catch (err) {
+      console.error('Error saving user data to Firestore:', err);
+    }
+  };
+
+  const saveUserDataToStorage = async (userData: IUser) => {
+    try {
+      await AsyncStorage.setItem('user', JSON.stringify(userData));
+    } catch (error) {
+      console.error('Error saving user data to storage:', error);
+    }
+  };
+
   const login = async () => {
-    setIsAuthLoading(true); // Set isAuthloading to true
+    setIsAuthLoading(true);
     try {
       await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
       const userInfo: SignInResponse = await GoogleSignin.signIn();
@@ -55,37 +123,53 @@ export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
       }
 
       const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-      await auth().signInWithCredential(googleCredential);
+      const userCredential = await auth().signInWithCredential(
+        googleCredential,
+      );
+      const userData = userCredential.user as CustomUser;
+
+      const userInfoToStore: IUser = {
+        id: userData.uid,
+        name: userData.displayName,
+        email: userData.email || '',
+        photo: userData.photoURL,
+        familyName: userData.familyName,
+        givenName: userData.givenName,
+      };
+
       setIsAuthenticated(true);
+      setUser(userInfoToStore);
+      await saveUserDataToStorage(userInfoToStore);
+      await saveUserDataToFirestore(userData);
+
       console.log('User signed in successfully');
     } catch (err: unknown) {
       if (err instanceof Error) {
-        if (err.message.includes(statusCodes.SIGN_IN_CANCELLED)) {
-          console.error('User cancelled the sign-in');
-        } else if (err.message.includes(statusCodes.IN_PROGRESS)) {
-          console.error('Sign-in in progress');
-        } else {
-          console.error('Google Sign-In Error:', err.message);
+        switch (err.message) {
+          case statusCodes.SIGN_IN_CANCELLED:
+            console.error('User cancelled the sign-in');
+            break;
+          case statusCodes.IN_PROGRESS:
+            console.error('Sign-in in progress');
+            break;
+          default:
+            console.error('Google Sign-In Error:', err.message);
         }
       } else {
         console.error('An unknown error occurred:', err);
       }
     } finally {
-      setIsAuthLoading(false); // Set isAuthloading to false after trying to login
+      setIsAuthLoading(false);
     }
   };
 
   const logout = async () => {
     try {
-      const currentUser = auth().currentUser;
-      if (!currentUser) {
-        console.warn('No user is currently signed in');
-        return;
-      }
-
       await auth().signOut();
       await GoogleSignin.signOut();
       setIsAuthenticated(false);
+      setUser(null);
+      await AsyncStorage.removeItem('user'); // Clear user data from AsyncStorage
     } catch (error) {
       console.error(
         'Logout Error:',
@@ -96,7 +180,7 @@ export const AuthProvider: React.FC<{children: ReactNode}> = ({children}) => {
 
   return (
     <AuthContext.Provider
-      value={{isAuthenticated, isAuthloading, login, logout}}>
+      value={{isAuthenticated, isAuthLoading, login, logout, user}}>
       {children}
     </AuthContext.Provider>
   );
