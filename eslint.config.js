d323460db1ee518288ae58ec25fa45c258fe9a38
pankaj-davi <menariapankaj17@gmail.com>
2024-10-02 15:21:59 +0530

    fix

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import globals from "globals";
+
+export default [
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                myCustomGlobal: "readonly"
+            }
+        },
+        rules: {
+            semi: ["warn", "always"]
+        }
+    }
+];
