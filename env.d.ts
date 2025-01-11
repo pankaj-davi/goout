2d02a38faf81a27f3b7a356b76aa9e8fa7be5c7e
pankaj-davi <menariapankaj17@gmail.com>
2025-01-11 07:27:31 +0530

    feat: add FIREBASE_STORAGE_BUCKET environment variable and update Firebase dependencies to latest version

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,7 @@
 declare module '@env' {
   export const FIRE_BASE_CLIENT_ID: string;
   export const API_URL: string; // Add other variables you expect to use here
+  export const FIREBASE_STORAGE_BUCKET: string; // Add other variables you expect to use here
+
   // Add additional environment variables as needed
 }
--- global.d.ts
+++ global.d.ts
@@ -5,4 +5,5 @@ declare module 'react-native-dotenv' {
   export const GOOGLE_API_KEY_webClientId: string;
   export const NODE_ENV: string;
   export const FIRE_BASE_CLIENT_ID: string;
+  export const FIREBASE_STORAGE_BUCKET: string;
 }
